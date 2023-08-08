c0482a734690fe9df0cf3cdb0ea17c8c3321514b
GnoM <gnomovi4@gmail.com>
2023-08-08 12:52:37 +0300

    error

--- terraform/VPC/3-subnets.tf
+++ terraform/VPC/3-subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnets)
 
   vpc_id = aws_vpc.this.id
-  cidr_block = "${var.public_subnets}"
+  cidr_block = "${var.vpc_cidr_block}"
   availability_zone = var.azs[count.index]
   
   tags = merge(
