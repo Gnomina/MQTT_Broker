e09c10490f39debe3462f6ca245f0031664bca22
GnoM <gnomovi4@gmail.com>
2023-08-08 12:57:26 +0300

    variable chng test

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "VPC" { #Include VPC module
   source = "./terraform/VPC" #VPC Module PATH
 
-  env = "Pico-dev"
-  azs = ["eu-central-1a"]
-  public_subnets = ["10.0.64.0/19"]
+  env = "Pico-test"
+  azs = ["eu-central-1a", "eu-central-1b"]
+  public_subnets = ["10.0.64.0/19", "10.0.94.0/19"]
 
   public_subnet_tags = {
-    "Pico-dev-demo" = "owned"
+    "Pico-dev-demo" = "test"
   }
 }
