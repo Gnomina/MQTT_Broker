4353bb253ff509566e0cbab83051fc762925d9d2
GnoM <gnomovi4@gmail.com>
2023-08-08 13:14:14 +0300

    work vpc

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "VPC" { #Include VPC module
   source = "./terraform/VPC" #VPC Module PATH
 
-  env = "Pico-test"
-  azs = ["eu-central-1a", "eu-central-1b"]
-  public_subnets = ["10.0.64.0/19", "10.0.96.0/19"]
+  env = "Pico-test" #enviorement name
+  azs = ["eu-central-1a"] # how many zones are needed
+  public_subnets = ["10.0.64.0/19"] # how many subnets are needed
 
   public_subnet_tags = {
     "Pico-dev-demo" = "test"
