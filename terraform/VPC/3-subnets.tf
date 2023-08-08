4d6ea2bc3706c914da3ff537eb1ec0785452c888
GnoM <gnomovi4@gmail.com>
2023-08-08 11:15:51 +0300

    igv error

--- terraform/VPC/3-subnets.tf
+++ terraform/VPC/3-subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_eu_central_1a" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.64.0/19"
-  availability_zone = "eu-central-1"
+  availability_zone = "eu-central-1a"
   
   tags = {
     Name = "pico-dev-subnet"
