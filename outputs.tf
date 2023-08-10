e4caf6e9282a028bdaa8ec4e2d4ce3837dd157a7
GnoM <gnomovi4@gmail.com>
2023-08-10 14:50:17 +0300

    inst_id add

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,7 @@ output "instance_public_ip" {
   value = module.instance.instance_public_ip #This variable locate in Instance module -> outputs 
   description = "public ip of instance from Instance module (outputs_source = ./terraform/instanse)"
 }
+
+output "instance_id" { # Local output variable
+  value = module.instance.instance_id # Instance ID that the instance receives from AWS. output variable.}
+}
