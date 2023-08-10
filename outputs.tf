140bc7edb79fb9c7b0a3f16befcab3affee7ccb2
GnoM <gnomovi4@gmail.com>
2023-08-10 12:25:29 +0300

    output_test

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 
 
-# output "instance_public_ip" {
-#   value = module.instanse.instance_public_ip #This variable locate in Instance module -> outputs 
-#   description = "public ip of instance from Instance module (outputs_source = ./terraform/instanse)"
-# }
+output "instance_public_ip" {
+  value = module.instanse.instance_public_ip #This variable locate in Instance module -> outputs 
+  description = "public ip of instance from Instance module (outputs_source = ./terraform/instanse)"
+}
